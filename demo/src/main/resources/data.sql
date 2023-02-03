ade740754cd87b6ac281548b6e4a4ea338c007c7
Yuzunoha <studysteadystandby@gmail.com>
2023-02-04 02:20:35 +0900

    複数行

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,1 +1,4 @@
-insert into `user` (user_name, email) values ('user太郎', 'user@example.com');
+insert into `user` (user_name, email) values
+    ('ユーザ1', 'user1@example.com'),
+    ('ユーザ2', 'user2@example.com'),
+    ('ユーザ3', 'user3@example.com');
