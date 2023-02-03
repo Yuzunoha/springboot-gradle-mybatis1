f1614383bcedf040379ac8fbc7582382aaf49c20
Yuzunoha <studysteadystandby@gmail.com>
2023-02-04 02:26:05 +0900

    http://localhost:8080/users/3

--- demo/src/main/java/com/example/demo/controllers/UserController.java
+++ demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -17,5 +18,10 @@ public class UserController {
   @GetMapping("/")
   public List<User> getAllUsers(Model model) {
     return service.selectAll();
+  }
+
+  @GetMapping("/users/{id}")
+  public User getUserById(@PathVariable Long id) {
+    return service.selectByPrimaryKey(id);
   }
 }
