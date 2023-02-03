accfbdb0b10e085c3c47678c2ace1b8873f33d0f
Yuzunoha <studysteadystandby@gmail.com>
2023-02-04 02:09:06 +0900

    保存

---
+++ demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.User;
+import com.example.demo.services.UserService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+  private final UserService service;
+
+  @GetMapping("/")
+  public List<User> getAllUsers(Model model) {
+    return service.selectAll();
+  }
+}
