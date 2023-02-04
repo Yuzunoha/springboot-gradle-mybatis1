2f49a35a6187d3b480d6d0697ea44aae2a52c314
Yuzunoha <studysteadystandby@gmail.com>
2023-02-05 02:47:48 +0900

    すごい。オブジェクトマッパーもDIされた

--- demo/src/main/java/com/example/demo/controllers/UserController.java
+++ demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -2,12 +2,18 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.User;
 import com.example.demo.services.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import java.util.function.UnaryOperator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -15,10 +21,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
   private final UserService service;
+  private final ObjectMapper objectMapper;
 
   @GetMapping("/")
   public List<User> getAllUsers(Model model) {
     return service.selectAll();
+  }
+
+  @PostMapping("/users")
+  public String addUser(@Validated @ModelAttribute User user, BindingResult result) {
+    try {
+      if (result.hasErrors()) {
+        return objectMapper.writeValueAsString(result.getAllErrors());
+      } else {
+        return objectMapper.writeValueAsString(user);
+      }
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      return null;
+    }
   }
 
   @GetMapping("/users/{id}")
