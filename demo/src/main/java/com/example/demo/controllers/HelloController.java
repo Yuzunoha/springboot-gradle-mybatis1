8316f6f699deb8f08022d42b99ebb1a2ce9b6d05
Yuzunoha <studysteadystandby@gmail.com>
2023-02-22 23:15:15 +0900

    Inner

--- demo/src/main/java/com/example/demo/controllers/HelloController.java
+++ demo/src/main/java/com/example/demo/controllers/HelloController.java
@@ -16,4 +16,36 @@ public class HelloController {
     String[] messages = { "こんにちは！", "", "お元気ですか？", "" };
     return Arrays.asList(messages);
   }
+
+  @GetMapping("/test")
+  public String test() {
+    Flyable f = Outer.create("太郎");
+    f.fly();
+    return "";
+  }
+}
+
+interface Flyable {
+  void fly();
+}
+
+class Outer {
+
+  private String name;
+
+  public Outer(String name) {
+    this.name = name;
+  }
+
+  public static Flyable create(String name) {
+    return (new Outer(name)).new Inner();
+  }
+
+  private class Inner implements Flyable {
+
+    @Override
+    public void fly() {
+      System.out.println(name + "が飛びます！！！");
+    }
+  }
 }
