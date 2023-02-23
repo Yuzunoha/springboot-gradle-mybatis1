a18e922a0f6caada7621d1be8378e4ee5155b478
Yuzunoha <studysteadystandby@gmail.com>
2023-02-23 18:50:35 +0900

    関数型インタフェースを実装した匿名クラスを書いた

--- demo/src/main/java/com/example/demo/services/LambdaYoyoService.java
+++ demo/src/main/java/com/example/demo/services/LambdaYoyoService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import com.example.demo.interfaces.SampleSum;
 import lombok.Getter;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,12 @@ public class LambdaYoyoService {
   private String url = "https://qiita.com/yoshitaro-yoyo/items/5a30915fc43a35ff10b4";
 
   public void run() {
-    System.out.println("runですyoyo");
+    SampleSum ss = new SampleSum() {
+      @Override
+      public int sum(int x, int y) {
+        return x + y;
+      }
+    };
+    System.out.println("runですyoyo: " + ss.sum(10, 3));
   }
 }
